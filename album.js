38accf9d7c473c29b553c0db53c0a1fe10b42de8
Christopher Bourke <cbourke@cse.unl.edu>
2015-08-03 20:15:51 -0500

    Initial Commit

---
+++ album.js
@@ -0,0 +1,15 @@
+
+function addAlbum() {
+
+}
+
+function addErrorBox(errorMessage) {
+  var errorDiv = '<div class="alert alert-danger alert-dismissible" role="alert">' +
+                 '<button type="button" class="close" data-dismiss="alert">' +
+                 '<span aria-hidden="true">&times;</span>' +
+                 '<span class="sr-only">Close</span></button>' +
+                 '<strong>ERROR!</strong> '+errorMessage+'</div>';
+  $('#errorArea').empty();
+  $('#errorArea').append(errorDiv);
+  $('#errorArea').hide().fadeIn("slow");
+}
