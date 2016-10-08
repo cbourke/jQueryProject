49eb94c020e004b9631956d6b075e5c8617d7089
Christopher Bourke <cbourke@cse.unl.edu>
2016-10-07 21:28:37 -0500

    added step-by-step instructions in comments

--- DynamicElements/main.js
+++ DynamicElements/main.js
@@ -1,14 +1,35 @@
 
-
-function reveal() {
-  //TODO
-}
-
 $(document).ready( function() {
+
   //TODO
+  //Place your page load animations here:
+
+
+
+  //Teaser Preview
+  //1. Use jQuery to get the HTML contents of the article and store it in a variable
+  //2. Use jQuery to get the text of the lead paragraph and store it in a variable
+  //3. Use strsub to get the first 50 characters of the lead paragraph text
+  //4. Create a string that consists of the first 50 characters, an ellipsis,
+  //   and an HTML formatted button with the proper onclick event
+  //5. Use jQuery to replace the HTML of the article with the string you created
+
 });
 
+/**
+ * This function restores and reveals the main article
+ */
+function reveal() {
+  //TODO
+  //Teaser Preview (continued)
+  //6. Restore the article contents to the original HTML
+  //7. Animate the restored article to give a visual cue to the user
+}
 
+/**
+ * This function collapses or expands all comments in the
+ * comment section.
+ */
 function toggleAllComments() {
   var text = $("#allToggle").text();
   var buttons;
@@ -27,6 +48,10 @@ function toggleAllComments() {
 
 }
 
+/**
+ *  This function collapses/expands a particular comment
+ *  in the comment section with the given commentNumber
+ */
 function commentToggle(commentNumber) {
   //TODO
 }
